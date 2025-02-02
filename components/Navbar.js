9cbe64d2209ddd4b9ecc16e1494fa87a57d59f20
Tarsh Swarnkar <2022kucp1032@iiitkota.ac.in>
2025-02-02 11:05:50 +0530

    v2

--- components/Navbar.js
+++ components/Navbar.js
@@ -95,20 +95,16 @@ export function Navbar() {
             </div>
           ) : null}
           <div className="flex items-center">
-            {session ? (
+          {session ? (
               <>
-                <a href="/complete-profile">
+                <a href={session.user.profileCompleted ? "/profile" : "/complete-profile"}>
                   <button className="text-gray-300 hover:text-white p-1 rounded-full hover:bg-dark transition-colors duration-200">
-                    <img
-                      src={session.user.image}
-                      alt="Profile"
-                      className="h-11 w-11 m-3 rounded-full object-cover"
-                    />
+                    <img src={session.user.image} alt="Profile" className="h-11 w-11 m-3 rounded-full object-cover" />
                   </button>
                 </a>
-                <button
-                  onClick={handleSignOut}
-                  disabled={isLoggingOut}
+                <button 
+                  onClick={handleSignOut} 
+                  disabled={isLoggingOut} 
                   className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gradient-to-r from-[#E31D65] to-[#FF6B2B] hover:opacity-90 transition-opacity duration-200"
                 >
                   {isLoggingOut ? 'Logging out...' : 'Logout'}
@@ -120,20 +116,9 @@ export function Navbar() {
                 className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gradient-to-r from-[#E31D65] to-[#FF6B2B] hover:opacity-90 transition-opacity duration-200"
               >
                 Login
-                {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="ml-2 -mr-1 h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg> */}
               </button>
             )}
+
           </div>
         </div>
       </div>
