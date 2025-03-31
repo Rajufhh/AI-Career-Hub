fc2a63c9e63dfc9db31afde803de10522de173ea
Tarsh Swarnkar <123285559+T1A0R3S2H@users.noreply.github.com>
2025-03-31 23:08:33 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -63,7 +63,7 @@ export function Navbar() {
   }
 
   return (
-    <nav className="bg-dark-lighter bg-opacity-50 backdrop-blur-sm sticky top-0 left-0 right-0 z-50 shadow-md">
+    <nav className="bg-dark-lighter bg-opacity-20 backdrop-blur-sm top-0 left-0 right-0 z-50 shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
